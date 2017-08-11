7620d711ec97279c4b42c6c2903750cd17102950
wujunfeng <wujunfeng@madhouse-inc.com>
2017-08-11 13:08:29 +0800

    fix bug: redisPool multi init

--- metadata/src/main/java/com/madhouse/platform/premiummad/util/ResourceManager.java
+++ metadata/src/main/java/com/madhouse/platform/premiummad/util/ResourceManager.java
@@ -33,24 +33,20 @@ public class ResourceManager {
     @Value("${redis.minIdle}")
     private Integer minIdle;
 
-    
     private JedisPool jedisPoolMaster;
-    
-//    private final ResourceManager resourceManager = new ResourceManager();
-    private ResourceManager(){};
-//    public ResourceManager getInstance() {
-//        return resourceManager;
-//    }
-    
-    public  JedisPool getJedisPoolMaster() {
-        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
-        poolConfig.setMinIdle(minIdle);
-        poolConfig.setMaxIdle(maxIdle);
-        poolConfig.setMaxTotal(timeout);
 
-        {
-            jedisPoolMaster = new JedisPool(poolConfig,redisHost, port);
+    public  JedisPool getJedisPoolMaster() {
+        synchronized (this) {
+            if (jedisPoolMaster == null) {
+                GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+                poolConfig.setMinIdle(minIdle);
+                poolConfig.setMaxIdle(maxIdle);
+                poolConfig.setMaxTotal(timeout);
+
+                jedisPoolMaster = new JedisPool(poolConfig,redisHost, port);
+            }
         }
+
         return jedisPoolMaster;
     }
     
