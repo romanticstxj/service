3ac2e2b734de68887ccc758e35212ea82d55875c
zhaoshengying <zhaoshengying@madhouse-inc.com>
2017-09-08 10:01:02 +0800

    update start.sh
    Signed-off-by: zhaoshengying <zhaoshengying@madhouse-inc.com>

--- metadata/src/main/resources/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "start metadata start ..."
-export JAVA_HOME=/services/apps/jdk
-export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
-
-nohup java -jar ../metadata.jar > /dev/null 2>/services/logs/service-metadata/log.txt &
