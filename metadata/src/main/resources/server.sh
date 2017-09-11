874c6e58e43f41a5bd87ecd42a189e30c5b9101a
zhaoshengying <zhaoshengying@madhouse-inc.com>
2017-09-11 12:01:55 +0800

    update server.sh
    Signed-off-by: zhaoshengying <zhaoshengying@madhouse-inc.com>

--- metadata/src/main/resources/server.sh
+++ metadata/src/main/resources/server.sh
@@ -2,13 +2,14 @@
 source /etc/profile
 
 BASE_DIR=/services/apps/service-metadata
-SYSTEM_LOG=/services/logs/service-metadata/log.txt
+#SYSTEM_LOG=/services/logs/service-metadata/log.txt
+SYSTEM_LOG=/dev/null
 SERVER_NAME="com.madhouse.platform.premiummad.task.MainTask"
 STARTUP_CLASS="com.madhouse.platform.premiummad.task.MainTask"
 CHECK_PID=`ps -ef|grep $SERVER_NAME |grep -v grep | awk '{print $2}' `
 
 #JVM
-#export JMX_PORT=9123
+export JMX_PORT=0912
 export CLASSPATH=$BASE_DIR/conf:$(ls $BASE_DIR/*.jar | tr '\n' :)
 
 #UEAP jvm args
@@ -41,10 +42,8 @@ start(){
 
     echo "BASE_DIR is :"$BASE_DIR
     echo "see result at:"$SYSTEM_LOG
-    nohup java $UEAP_JVM_ARGS  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote.authenticate=false 
-    -Dcom.sun.management.jmxremote.ssl=false 
-#	-Dcom.sun.management.jmxremote.port=$JMX_PORT 
-	$STARTUP_CLASS > /dev/null 2 > $SYSTEM_LOG  
+    nohup java $UEAP_JVM_ARGS  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false \
+-Dcom.sun.management.jmxremote.port=$JMX_PORT $STARTUP_CLASS > $SYSTEM_LOG & 
     sleep 1;
     exit
 }
