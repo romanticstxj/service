8634f8081809f57bf92803a696d41eb562e3434f
陈绵慧 <chenmianhui@madhouse-inc.com>
2017-11-14 16:20:35 +0800

    1. 开始日期移除与当前日期比较的校验 2. DSP素材上传相关接口加日志 3. 修复bug

--- materials/src/main/java/com/madhouse/platform/premiummad/exception/CommonExceptionHandler.java
+++ materials/src/main/java/com/madhouse/platform/premiummad/exception/CommonExceptionHandler.java
@@ -5,10 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
 import com.madhouse.platform.premiummad.constant.StatusCode;
-import com.madhouse.platform.premiummad.constant.SystemConstant;
 import com.madhouse.platform.premiummad.dto.ResponseDto;
 import com.madhouse.platform.premiummad.util.LogUtils;
 import com.madhouse.platform.premiummad.util.ResponseUtils;
@@ -19,18 +18,23 @@ import com.madhouse.platform.premiummad.util.ResponseUtils;
 @ControllerAdvice
 public class CommonExceptionHandler {
 	
-	private static final Logger logger = LoggerFactory.getLogger(SystemConstant.Logging.LOGGER_PREMIUMMAD_ERROR);
+	private static final Logger LOGGER = LoggerFactory.getLogger(CommonExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
 	@ResponseBody
 	public ResponseDto<Object> handleAllException(Exception ex) {
-		logger.error(LogUtils.getDetailException(ex));
+		ResponseDto<Object> response = null;
 		if (ex instanceof BusinessException) {
-			return ResponseUtils.response(((BusinessException) ex).getStatusCode(), null, ex.getMessage());
+			response = ResponseUtils.response(((BusinessException) ex).getStatusCode(), null, ex.getMessage());
 		} else if (ex instanceof JSONException) {
-			return ResponseUtils.response(StatusCode.SC400);
+			LOGGER.error(LogUtils.getDetailException(ex));
+			response = ResponseUtils.response(StatusCode.SC400);
 		} else {
-			return ResponseUtils.response(StatusCode.SC500, null, ex.getMessage());
+			LOGGER.error(LogUtils.getDetailException(ex));
+			response = ResponseUtils.response(StatusCode.SC500, null, ex.getMessage());
 		}
+		
+		LOGGER.info("DSP Exception-{}", JSON.toJSONString(response));
+		return response;
 	}
 }
